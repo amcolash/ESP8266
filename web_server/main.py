d27ec40594246f7a80d2b9af120cb35d659a69e3
Andrew McOlash <amcolash@gmail.com>
2016-12-28 16:14:00 -0600

    Modified web server slightly

--- web_server/main.py
+++ web_server/main.py
@@ -1,9 +1,6 @@
-import machine
+from machine import Pin
 import socket
-
-pins = [machine.Pin(i, machine.Pin.OUT) for i in (0, 2, 4, 5, 12, 13, 14, 15)]
-
-pins[1].value(1)
+import time
 
 html = """<!DOCTYPE html>
 <html>
@@ -15,30 +12,6 @@ html = """<!DOCTYPE html>
 """
 
 
-import uasyncio as asyncio
-
-@asyncio.coroutine
-def serve(reader, writer):
-    print(reader, writer)
-    print("================")
-    print((yield from reader.read()))
-    yield from writer.awrite("HTTP/1.0 200 OK\r\n\r\nHello.\r\n")
-    print("After response write")
-    yield from writer.aclose()
-    print("Finished processing request")
-
-
-import logging
-#logging.basicConfig(level=logging.INFO)
-logging.basicConfig(level=logging.DEBUG)
-loop = asyncio.get_event_loop()
-loop.call_soon(asyncio.start_server(serve, "0.0.0.0", 80))
-loop.run_forever()
-loop.close()
-
-print('all done')
-
-'''
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 
 s = socket.socket()
@@ -47,8 +20,9 @@ s.listen(1)
 
 print('listening on', addr)
 
+button_0 = Pin(0, Pin.IN)
 
-while True:
+while (button_0.value() == 1):
     cl, addr = s.accept()
     print('client connected from', addr)
     cl_file = cl.makefile('rwb', 0)
@@ -56,9 +30,11 @@ while True:
         line = cl_file.readline()
         if not line or line == b'\r\n':
             break
-    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
-    response = html % '\n'.join(rows)
+    #rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+    #response = html % '\n'.join(rows)
+    response = html
     cl.send(response)
     cl.close()
     cl_file.close()
-'''
+
+#red_led.value(0)
