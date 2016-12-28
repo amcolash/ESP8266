6db1f643a2b116035d23550fb742fc355180d490
Andrew McOlash <amcolash@gmail.com>
2016-12-28 16:16:13 -0600

    Updated boot.py

--- web_server/boot.py
+++ web_server/boot.py
@@ -4,22 +4,29 @@
 #import webrepl
 #import gc
 import network
-import machine
+import time
 
 #esp.osdebug(None)
 #webrepl.start()
 
+SSID='mcolash'
+PASS=''
+
+AP_ENABLED = True
+CONNECT_ENABLED = False
+
 ap_if = network.WLAN(network.AP_IF)
-ap_if.active(False)
+ap_if.active(AP_ENABLED)
 
 
 sta_if = network.WLAN(network.STA_IF)
-if not sta_if.isconnected():
+sta_if.active(CONNECT_ENABLED)
+if CONNECT_ENABLED and not sta_if.isconnected():
     print('connecting to network...')
-    sta_if.active(True)
-    sta_if.connect('mcolash', '')
+    sta_if.connect(SSID, PASS)
     while not sta_if.isconnected():
         pass
+
     print('network config:', sta_if.ifconfig())
 
 gc.collect()
