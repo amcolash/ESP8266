b5ff6b465105340ab6dcf9002e6f9c45c06a415b
Andrew McOlash <amcolash@gmail.com>
2018-09-30 19:46:20 -0700

    Add in IR send/recieve

--- wakeup/wakeup.ino
+++ wakeup/wakeup.ino
@@ -3,8 +3,23 @@
 #include <ESP8266WebServer.h>
 #include <ArduinoOTA.h>
 
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRsend.h>
+#include <IRutils.h>
+
 const char* ssid = "Obi-LAN Kenobi";
 const char* password = "UseTheForce";
+
+uint64_t OFF_CODE = 0x4CB3748B;
+uint64_t ON_CODE = 0x4CB340BF;
+
+const int RECV_PIN = 12;
+const int SEND_PIN = 13;
+
+IRrecv irrecv(RECV_PIN);
+IRsend irsend(SEND_PIN);
+decode_results results;
 
 ESP8266WebServer server(80);
 
@@ -12,10 +27,22 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
 
+  setupPins();
+
+  Serial.begin(115200,SERIAL_8N1,SERIAL_TX_ONLY);
+
+  // Turn on red LED to indicate setup
+  digitalWrite(0, 0);
+  
   setupWifi();
   setupOTA();
   setupServer();
-  setupPins();
+
+  irrecv.enableIRIn();
+  irsend.begin();
+
+  // Setup done, turn off LED
+  digitalWrite(0, 1);
   
   Serial.println("Ready");
 }
@@ -40,6 +67,7 @@ void setupWifi() {
 
 void setupOTA() {
   ArduinoOTA.onStart([]() {
+    digitalWrite(0, 0);
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH)
       type = "sketch";
@@ -51,6 +79,7 @@ void setupOTA() {
   });
   
   ArduinoOTA.onEnd([]() {
+    digitalWrite(0, 1);
     Serial.println("\nEnd");
   });
   
@@ -95,10 +124,13 @@ void setupServer() {
 }
 
 void setupPins() {
-  // Pin 2, LED
+  // Pin 0, LED (RED)
+  // Pin 2, LED (BLUE)
   // Pin 4, Power LED
   // Pin 5, Power Switch
+  // Pin 12, IR
 
+  pinMode(0, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(4, INPUT);
   pinMode(5, OUTPUT);
@@ -111,7 +143,16 @@ void loop() {
   ArduinoOTA.handle();
   server.handleClient();
 
-  digitalWrite(2, digitalRead(4));
+  if (irrecv.decode(&results)) {
+    // print() & println() can't handle printing long longs. (uint64_t)
+    serialPrintUint64(results.value, HEX);
+    Serial.println("");
+    irrecv.resume();  // Receive the next value
+
+    irsend.sendNEC(ON_CODE);
+  }
+
+//  digitalWrite(2, digitalRead(4));
 }
 
 void changePower(bool on, bool forced) {
@@ -128,7 +169,7 @@ void changePower(bool on, bool forced) {
     server.send(200, "text/plain", response);
 
     digitalWrite(5, 1);
-    delay(forced ? 5000 : 500);
+    delay(forced ? 5000 : 1000);
     digitalWrite(5, 0);
   } else {
     response += "Nothing Done";
