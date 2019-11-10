32ae8a60280e8d948346d6688b498f87f403f603
Andrew McOlash <amcolash@gmail.com>
2019-11-10 15:35:30 -0800

    Add in time and wifi icon

--- led_spectrum/led_spectrum.ino
+++ led_spectrum/led_spectrum.ino
@@ -2,6 +2,18 @@
 #include <PxMatrix.h>
 #include <Ticker.h>
 #include <fix_fft.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+
+// Set up wifi configuration
+const char *ssid = "Obi-LAN Kenobi";
+const char *pass = "UseTheForce";
+
+// NTP Client
+const long utcOffsetInSeconds = -8 * 60 * 60;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, utcOffsetInSeconds);
 
 #define P_LAT 16
 #define P_A 5
@@ -22,6 +34,24 @@ uint16_t CYAN = display.color565(0, 255, 255);
 uint16_t BLACK = display.color565(0, 0, 0);
 uint16_t LIME = display.color565(20, 255, 20);
 
+// Wifi icon by Gregbaker, from wikimedia: https://commons.wikimedia.org/wiki/File:Wireless-icon.svg
+// 'wifi', 26x24px
+const unsigned char wifiIcon [] PROGMEM = {
+  0x00, 0x3f, 0x00, 0x00, 0x01, 0xff, 0xf0, 0x00, 0x0f, 0xff, 0xfc, 0x00, 0x1f, 0xe1, 0xfe, 0x00, 
+  0x3f, 0x00, 0x1f, 0x80, 0x7c, 0x00, 0x0f, 0xc0, 0xf0, 0x7f, 0x83, 0xc0, 0x61, 0xff, 0xf1, 0x80, 
+  0x07, 0xff, 0xf8, 0x00, 0x0f, 0xc0, 0xfc, 0x00, 0x0f, 0x00, 0x3c, 0x00, 0x06, 0x1e, 0x08, 0x00, 
+  0x00, 0x7f, 0x80, 0x00, 0x00, 0xff, 0xc0, 0x00, 0x00, 0xff, 0xc0, 0x00, 0x00, 0x60, 0xc0, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x1e, 0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 
+  0x00, 0x3f, 0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 0x00, 0x1e, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+// 'wifi_small', 16x15px
+const unsigned char wifiIconSmall [] PROGMEM = {
+  0x03, 0xc0, 0x1f, 0xf8, 0x78, 0x1e, 0xf0, 0x07, 0xcf, 0xf3, 0x1f, 0xf8, 0x38, 0x1c, 0x13, 0xc8, 
+  0x07, 0xe0, 0x06, 0x60, 0x00, 0x00, 0x03, 0xc0, 0x03, 0xc0, 0x03, 0xc0, 0x00, 0x00
+};
+
+
 byte RedLight;
 byte GreenLight;
 byte BlueLight;
@@ -41,6 +71,7 @@ void display_updater() {
 void setup() {
   Serial.begin(115200);
 
+  // Init display
   display.begin(16);
   display.setFastUpdate(true);
   display.flushDisplay();
@@ -48,7 +79,35 @@ void setup() {
   // Paint to display 500x a second
   display_ticker.attach(0.002, display_updater);
 
-  for (i=0; i<64; i++) { prev[i]=0; };
+  // Draw wifi connecting icon
+//  display.drawBitmap(19, 4, wifiIcon, 26, 24, LIME);
+  display.drawBitmap(24, 9, wifiIconSmall, 16, 15, LIME);
+  display.showBuffer();
+
+  // Start up wifi
+  Serial.print(F("\nConnecting to: "));
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA); //We don't want the ESP to act as an AP
+  WiFi.begin(ssid, pass); // Connect to the configured AP
+
+  // No point moving forward if we don't have a connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+
+  // We have connected to wifi successfully
+  Serial.println(F("\nWiFi connected"));
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Set up NTP client
+  Serial.println("Setting up NTP");
+  timeClient.begin();
+
+  for (i=0; i < 64; i++) { prev[i] = 0; };
   for (i=0; i < BARS; i++) { lines[i] = 0; };
 }
 
@@ -57,8 +116,20 @@ float mixFactor = 0.25;
 float colorOffset = 0;
 int idle = 0;
 int averageIdle = 0;
+char timeDisplay[10];
+
+// roughly 50 updates a second * 60 sec * 60 min * 8 hour
+long ntpUpdateTime = 50 * 60 * 60 * 8;
+long ntpCount = ntpUpdateTime;
 
 void loop() {
+  // Updating with ntp is costly and makes the display freeze for a moment, only update once in a while
+  ntpCount++;
+  if (ntpCount > ntpUpdateTime) {
+    timeClient.update();
+    ntpCount = 0;
+  }
+  
   int total = 0;
   for (i=0; i < 128; i++){
 //    microseconds = micros();
@@ -109,7 +180,21 @@ void loop() {
 //    display.fillRect(i*BAR_SIZE, 16, BAR_SIZE, 16, color);
 
     lines[i] = 0;
-  };
+  }
+
+  char* am = "AM";
+  int hour = timeClient.getHours();
+  if (hour >= 12) {
+    hour -= 12;
+    am = "PM";
+  }
+  int minute = timeClient.getMinutes();
+
+  sprintf(timeDisplay, "%d:%02d%s", hour, minute, am);
+  
+  display.setTextColor(LIME);
+  display.setCursor((64 - (strlen(timeDisplay) * 6)) / 2, 4);
+  display.print(timeDisplay);
   
   display.showBuffer();
 //  colorOffset += 0.01;
