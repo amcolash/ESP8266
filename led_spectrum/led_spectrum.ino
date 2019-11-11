91bcb5c917bd061e349d9e88ce54c4fd049360ca
Andrew McOlash <amcolash@gmail.com>
2019-11-10 17:32:56 -0800

    Remove a bunch of old code - hopefully mostly cleaned up now

--- led_spectrum/led_spectrum.ino
+++ led_spectrum/led_spectrum.ino
@@ -50,19 +50,8 @@ PxMATRIX display(64,32,P_LAT, P_OE,P_A,P_B,P_C,P_D);
 uint16_t BLACK = display.color565(0, 0, 0);
 uint16_t LIME = display.color565(20, 255, 20);
 
-// Wifi icon by Gregbaker, from wikimedia: https://commons.wikimedia.org/wiki/File:Wireless-icon.svg
-// 'wifi', 26x24px
-const unsigned char wifiIcon [] PROGMEM = {
-  0x00, 0x3f, 0x00, 0x00, 0x01, 0xff, 0xf0, 0x00, 0x0f, 0xff, 0xfc, 0x00, 0x1f, 0xe1, 0xfe, 0x00, 
-  0x3f, 0x00, 0x1f, 0x80, 0x7c, 0x00, 0x0f, 0xc0, 0xf0, 0x7f, 0x83, 0xc0, 0x61, 0xff, 0xf1, 0x80, 
-  0x07, 0xff, 0xf8, 0x00, 0x0f, 0xc0, 0xfc, 0x00, 0x0f, 0x00, 0x3c, 0x00, 0x06, 0x1e, 0x08, 0x00, 
-  0x00, 0x7f, 0x80, 0x00, 0x00, 0xff, 0xc0, 0x00, 0x00, 0xff, 0xc0, 0x00, 0x00, 0x60, 0xc0, 0x00, 
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x1e, 0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 
-  0x00, 0x3f, 0x00, 0x00, 0x00, 0x3f, 0x00, 0x00, 0x00, 0x1e, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-};
-
 // 'wifi_small', 16x15px
-const unsigned char wifiIconSmall [] PROGMEM = {
+const unsigned char wifiIcon [] PROGMEM = {
   0x03, 0xc0, 0x1f, 0xf8, 0x78, 0x1e, 0xf0, 0x07, 0xcf, 0xf3, 0x1f, 0xf8, 0x38, 0x1c, 0x13, 0xc8, 
   0x07, 0xe0, 0x06, 0x60, 0x00, 0x00, 0x03, 0xc0, 0x03, 0xc0, 0x03, 0xc0, 0x00, 0x00
 };
@@ -76,7 +65,15 @@ byte BlueLight;
 int BAR_SIZE = 64 / BARS;
 
 int8_t data[128], prev[64], lines[BARS], lastLines[BARS];
-int i=0,val;
+int i,val;
+
+float mixFactor = 0.25;
+char timeDisplay[10];
+int temperature = -999;
+
+// roughly 50 updates a second * 60 sec * 60 min (every hour)
+long updateTime = 50 * 60 * 60;
+long updateCount = updateTime;
 
 
 Ticker display_ticker;
@@ -98,7 +95,7 @@ void setup() {
 
   // Draw wifi connecting icon
 //  display.drawBitmap(19, 4, wifiIcon, 26, 24, LIME);
-  display.drawBitmap(24, 9, wifiIconSmall, 16, 15, LIME);
+  display.drawBitmap(24, 9, wifiIcon, 16, 15, LIME);
   display.showBuffer();
 
   // Start up wifi
@@ -129,18 +126,6 @@ void setup() {
   for (i=0; i < 64; i++) { prev[i] = 0; };
   for (i=0; i < BARS; i++) { lines[i] = 0; };
 }
-
-unsigned long microseconds;
-float mixFactor = 0.25;
-float colorOffset = 0;
-int idle = 0;
-int averageIdle = 0;
-char timeDisplay[10];
-int temperature = -999;
-
-// roughly 50 updates a second * 60 sec * 60 min (every hour)
-long updateTime = 50 * 60 * 60;
-long updateCount = updateTime;
 
 void updateWeather() {
   http.begin(openweatherEndpoint  + openweatherKey); //Specify the URL
@@ -186,12 +171,9 @@ void loop() {
     if (hour >= (evening + 12)) { display.setBrightness(200); }
     else { display.setBrightness(255); }
   }
-
   
   int total = 0;
   for (i=0; i < 128; i++){
-//    microseconds = micros();
-
     val = (analogRead(A0) >> 2) - 128;  // fit to 8 bit int
     data[i] = val;
     total += val;
@@ -209,33 +191,23 @@ void loop() {
 
   fix_fftr(data,7,0);
   
-  for (i=0; i < 64;i++){
+  for (i=0; i < 64; i++){
     if(data[i] < 0) data[i] = 0;
     else data[i] *= SCALE_FACTOR;
-//    else data[i] *= log_scale*log((float)(data[i]+1));
-
-//    data[i] = data[i] * mixFactor + prev[i] * (1. - mixFactor);
-//    prev[i] = data[i];
-
-//    display.drawFastVLine(i, 32 - data[i], data[i], LIME);
 
     lines[i / BAR_SIZE] += (data[i] / BAR_SIZE);
   }
 
   display.fillScreen(BLACK);
+  
   for (i=0; i < BARS; i++) {
     lines[i] = max((float) 1, lines[i] * mixFactor + lastLines[i] * (1 - mixFactor));
     lastLines[i] = lines[i];
 
-//    setLedColorHSV((int) ((i - colorOffset) / BARS * 256) % 256, 255 - ((lines[i] / 32.) * 128), 255);
-//    setLedColorHSV((int) ((i - colorOffset) / BARS * 256) % 256, lines[i] + 224, 255);
-    setLedColorHSV((int) ((i - colorOffset) / BARS * 256) % 256, 255, 255);
-    
+    setLedColorHSV((int) ((float) i / BARS * 255) % 255, 255, 255);
     uint16_t color = display.color565(RedLight, GreenLight, BlueLight);
     
-//    display.drawFastHLine(i*BAR_SIZE, 32 - min(lines[i] + 0, 16), BAR_SIZE, color);
     display.fillRect(i*BAR_SIZE, 32 - min(lines[i] + 0, 16), BAR_SIZE, lines[i], color);
-//    display.fillRect(i*BAR_SIZE, 16, BAR_SIZE, 16, color);
 
     lines[i] = 0;
   }
@@ -275,8 +247,7 @@ void loop() {
   }
   
   display.showBuffer();
-//  colorOffset += 0.01;
-
+  
   delay(20);
 }
 
