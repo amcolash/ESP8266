226c187bd31efe603dd8c2a3a33008c64aee4491
Andrew McOlash <amcolash@gmail.com>
2025-02-23 00:40:36 -0800

    chile lights ISR for flicker free flipping and slowly fade to target

--- chile_lights/chile_lights.ino
+++ chile_lights/chile_lights.ino
@@ -7,9 +7,6 @@
 
 #define BROKER_ADDR     IPAddress(192, 168, 1, 101)
 
-uint16_t count = 0;
-bool flip = false;
-
 WiFiManager wifiManager;
 WiFiClient client;
 
@@ -18,8 +15,18 @@ HAMqtt mqtt(client, device);
 
 HALight light("ChileLights", HALight::BrightnessFeature);
 
-volatile bool on = false;
-volatile uint8_t brightness = 1;
+// count how far through pwm cycle the ISR is
+volatile uint8_t pwmCounter = 0;
+// whether to set IN1 or IN2, flip the other
+volatile bool flip = false;
+
+// if the light state is on or off
+volatile bool on = true;
+
+// current value shown on leds
+volatile uint8_t current = 255;
+// target brightness and value reported to HA
+uint8_t brightness = 255;
 
 void onStateCommand(bool state, HALight* sender) {
   on = state;
@@ -37,20 +44,27 @@ void onBrightnessCommand(uint8_t newBrightness, HALight* sender) {
   Serial.println(newBrightness);
 }
 
-void flipLights() {
-  if (on) {
+// Flip light pin output to work with motor driver. Cannot use analogWrite for pwm, since
+// we already need to use Timer1 for this interrupt. Therefore, we roll our own pwm as well.
+void IRAM_ATTR flipLights() {
+  pwmCounter++;
+
+  // If "active" side of pwm, go high
+  if (pwmCounter < current) {
     if (flip) {
-      analogWrite(IN1_PIN, brightness);
-      analogWrite(IN2_PIN, 0);
+      digitalWrite(IN1_PIN, HIGH);
+      digitalWrite(IN2_PIN, LOW);
     } else {
-      analogWrite(IN1_PIN, 0);
-      analogWrite(IN2_PIN, brightness);
+      digitalWrite(IN1_PIN, LOW);
+      digitalWrite(IN2_PIN, HIGH);
     }
 
+    // next cycle will be flipped from this one
     flip = !flip;
   } else {
-    analogWrite(IN1_PIN, 0);
-    analogWrite(IN2_PIN, 0);
+    // Low at end of cycle
+    digitalWrite(IN1_PIN, LOW);
+    digitalWrite(IN2_PIN, LOW);
   }
 }
 
@@ -64,6 +78,11 @@ void setup() {
 
   digitalWrite(IN1_PIN, LOW);
   digitalWrite(IN2_PIN, LOW);
+
+  // set up Timer1 ISR to flip light output back and forth
+  timer1_attachInterrupt(flipLights);
+  timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP);
+  timer1_write(100);
 
   // connect to wifi
   wifiManager.autoConnect("ESP-ChileLights");
@@ -87,11 +106,36 @@ void setup() {
   light.onStateCommand(onStateCommand);
   light.onBrightnessCommand(onBrightnessCommand); // optional
 
+  // begin mqtt broker
   mqtt.begin(BROKER_ADDR);
+
+  // wait until mqtt has connected
+  while (!mqtt.isConnected()) {
+    mqtt.loop();  // Process MQTT connection
+    delay(100);
+  }
+
+  // force the state of the lights to default values
+  light.setState(on);
+  light.setBrightness(brightness);
 }
+
+uint8_t counter = 0;
 
 void loop() {
   mqtt.loop();
 
-  flipLights();
+  // Counter limits the speed of fading
+  if (counter == 0) {
+    // If turning off, slowly fade off
+    if (!on) {
+       if (current > 0) current--;
+    }
+    // If normal, slowly moving current brightness closer to target
+    else if (current < brightness) current++;
+    else if (current > brightness) current--;
+  }
+
+  // count up slowly to make fading more gradual
+  counter = (counter + 1) % 127;
 }
